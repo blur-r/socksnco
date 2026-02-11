3bea0a69857b4447a92ffea377c97af122cfb723
Babalola Paul <pbabalola27@gmail.com>
2026-02-11 18:24:53 +0100

    made the navigation stick

--- socksncoo/src/App.jsx
+++ socksncoo/src/App.jsx
@@ -4,23 +4,26 @@ import ProductCard from './component/ProductCard'
 function App() {
   return (
     <>
-      <div className="min-h-svh bg-contain bg-center bg-no-repeat bg-[#FBC322]" style={{ backgroundImage: `url(${bgImage})` }}>
-        <div className='flex gap-4 bg-white w-40 md:w-50 py-3 px-3 justify-center mx-auto rounded-b-full sticky'>
+      <div className='bg-[#FBC322] sticky top-0 z-50'>
+        <div className='flex gap-4 bg-white w-40 md:w-50 py-3 px-3 justify-center mx-auto rounded-b-full shadow-[0_0_11px_-1px_rgba(0,0,0,0.25)]'>
           <a href="#">
-            <i class="fa-solid fa-cart-shopping text-2xl md:text-3xl"></i>
+            <i className="fa-solid fa-cart-shopping text-2xl md:text-3xl"></i>
           </a>
           <a href="#">
-            <i class="fa-solid fa-heart text-2xl md:text-3xl"></i>
+            <i className="fa-solid fa-heart text-2xl md:text-3xl"></i>
           </a>
           <a href="#">
-            <i class="fa-solid fa-envelope text-2xl md:text-3xl"></i>
+            <i className="fa-solid fa-envelope text-2xl md:text-3xl"></i>
           </a>
         </div>
+      </div>
+      <div className="min-h-svh bg-contain bg-center bg-no-repeat bg-[#FBC322]" style={{ backgroundImage: `url(${bgImage})` }}>
+
       </div>
 
       <div className='flex flex-wrap justify-center gap-5 md:gap-10 my-10 '>
 
-        <div className='bg-[#f2f2f2] w-40 h-14 md:w-40 md:h-40 py-6 rounded-sm md:rounded-2xl flex justify-evenly md:justify-center items-center md:flex-col md:gap-2 shadow-[0_0_11px_-1px_rgba(0,0,0,0.25)]'>
+        <div className='bg-[#F3F4F6] w-40 h-14 md:w-40 md:h-40 py-6 rounded-sm md:rounded-2xl flex justify-evenly md:justify-center items-center md:flex-col md:gap-2 shadow-[0_0_11px_0px_rgba(0,0,0,0.25)]'>
           <div className='bg-[#f9f0d5] rounded-full w-12 h-12 md:w-20 md:h-20 flex justify-center items-center'>
             <i class="fa-solid fa-socks text-xl md:text-3xl text-[#FBC322]"></i>
           </div>
