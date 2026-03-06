a7aec09ff1fa69dbff842e9bb6cfca2ccde98923
Babalola Paul <pbabalola27@gmail.com>
2026-03-06 19:25:34 +0100

    update Footer component with improved layout and content

--- socksncoo/src/component/Footer.jsx
+++ socksncoo/src/component/Footer.jsx
@@ -1,8 +1,16 @@
 const Footer = () => {
     return (
         <>
-            <div className="bg-[#FBC322] h-20 flex items-center justify-center text-white text-sm font-semibold">
-                footer
+            <div className="bg-[#FBC322] h-30 flex items-center justify-center text-white text-sm font-semibold">
+                <div>
+                    <p className="text-center">
+                        thank you for shopping with us! we hope you have a great day!
+                    </p>
+                    <p className="text-center">
+                        @2026 socksncoo. all rights reserved.
+                    </p>
+
+                </div>
             </div>
         </>
     )
